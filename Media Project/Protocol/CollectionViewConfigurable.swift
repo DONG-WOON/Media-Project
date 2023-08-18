f3b98401d925fcede713c7156810ce936abaf670
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-08-18 23:56:35 +0900

    [feat] protocol CollectionViewConfigurable

---
+++ Media Project/Protocol/CollectionViewConfigurable.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewConfigurable.swift
+//  Media Project
+//
+//  Created by 서동운 on 8/18/23.
+//
+
+import Foundation
+
+protocol CollectionViewConfigurable {
+    func configureCollectionViewLayout()
+    func configureCollectionView()
+}
