2a5baac8d32383ffd5a075a32d0c483f200942cf
domb <80871083+DONG-WOON@users.noreply.github.com>
2023-08-25 20:42:11 +0900

    [feat] add TabBarController and SearchViewController

---
+++ Media Project/Search/SearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.swift
+//  Media Project
+//
+//  Created by 서동운 on 8/23/23.
+//
+
+import UIKit
+
+class SearchViewController: UISearchController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+}
